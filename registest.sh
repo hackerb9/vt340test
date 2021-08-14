38a6db54cc0c5b6d5464344206e328d49c1cb70f
b9 <hackerb9@member.fsf.org>
2021-08-14 00:08:10 -0700

    remove obsolete Media Copy comment
    mediacopy.sh is its own script now, not embedded in registest.sh

--- registest.sh
+++ registest.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #
-# Use REGIS to draw some graphics on the VT340 screen and then use the
-# MC (Media Copy) command to read the graphics back as Sixel data.
+# Use REGIS to draw some graphics on the VT340 screen
 
 CSI=$'\e['			# Control Sequence Introducer
 DCS=$'\eP'			# Device Control String
@@ -9,15 +8,8 @@ ST=$'\e\\'			# String Terminator
 
 clear
 
-# DECGPCM: Print Graphics Color Mode
-#echo -n ${CSI}'?44l'		# Print in black and white
-echo -n ${CSI}'?46h'		# Print in Color
-
-# DECGPBM: Print Graphics Background Mode
-echo -n ${CSI}'?46l'		# Do not send background when printing
-#echo -n ${CSI}'?46h'		# Include background when printing
-
 echo -n ${DCS}'3p'		# Enter interactive REGIS mode
+#echo -n $'S(M(
 echo -n $'P[400,200]W(S1)C(W(I1))[+100]C[+66]C(W(I2))[+33]' # RAF roundels
 echo -n ${ST}			# Exit REGIS mode
 
