71f6cfeb0cde918d84677c100571d78491e827fc
b9 <hackerb9@member.fsf.org>
2021-09-14 07:20:08 -0700

    Show difference between text newline and graphics newline

--- sixeltests/cursor.sh
+++ sixeltests/cursor.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+# Test cursor placement after sixel image is sent.
+
 # After a sixel image is sent, the text cursor is moved down
-# to the row which contained the last sixels drawn. The column
-# does not change.
+# to the row which contained the last sixel cursor position.
+# The column does not change.
+
+# Sixel images often do *not* end with a `-` (Graphics NewLine) which
+# sends the sixel cursor down 6 pixels and to the left edge of the graphic.
+
+# Anything printed next will potentially overlap the last row of sixels!
+# In general, a text newline (^J) is sent by the application after
+# showing a sixel image and before sending more text or graphics.
+
+# However, it is important to note that a newline is not always wanted.
+# For example, if an image is full screen, a newline would cause the
+# top line to scroll off the screen.
 
 
 CSI=$'\e['			# Control Sequence Introducer 
@@ -23,23 +36,78 @@ square() {
     # square with top left corner at (row, column) and of size 100px by 100px.
     # (10cols, 5 rows)
 
-
     local -i color=$1 row=$2 column=$3
-    if [[ row != 0 && column != 0 ]]; then
+    if [[ row -ne 0 && column -ne 0 ]]; then
 	set_cursor_pos $row $column
     fi
+	
+    echo -n ${DCS}'0;0;0q"1;1;100;100#'${color}'!100~-!100~-!100~-!100~-!100~-!100~-!100~-!100~-!100~-!100~-!100~-!100~-!100~-!100~-!100~-!100~-!100N'${ST}
+
+}
+
+squaregnl() {
+    # Same as square(), but sends a graphics newline at the end of the sixels.
+
+    local -i color=$1 row=$2 column=$3
+    if [[ row -ne 0 && column -ne 0 ]]; then
+	set_cursor_pos $row $column
+    fi
+
     echo -n ${DCS}'0;0;0q"1;1;100;100#'${color}'!100~-!100~-!100~-!100~-!100~-!100~-!100~-!100~-!100~-!100~-!100~-!100~-!100~-!100~-!100~-!100~-!100N-'${ST}
 
 }
 
 
-# Blue square covers columns 36 to 45 (10 char wide) and rows 10 to 14 
-square 1 10 36
+clear
 
-# Red square spans column 76 to 85   --- vt340 cuts of at column 80
+# TYPICAL sixel images do not end with a Graphics New Line (GNL)
+
+# Blue square covers columns 26 to 35 (10 char wide) and rows 5 to 9
+square 1 5 26
+
+# Red square should cover the same columns, spanning rows 9 to 13
+# OVERLAPPING last row of blue!
 square 2
 
-# Green square spans rows 23 to 27   --- vt340 scrolls rows 1 to 24 up
-square 23 35 3
+# Green square, overlaps red, rows 13 to 17
+square 3
    
-read -t 5
+# Draw words overlapping the bottom of the green square
+echo -n "Overlap"
+
+
+# USING A TEXT NEWLINE (NL) after an sixel image that does NOT have GNL 
+# is the only guaranteed way to be on the text line immediately below
+# the image. Text newline always sends to the cursor to column 1, however.
+
+# Pastel blue square covers columns 1 to 9 and rows 5 to 9
+square 9 5 1 
+echo				# Text newline
+
+# Pastel red is below blue, rows 10 to 14, no overlap.
+square 10
+echo
+
+# Pastel green is below red, rows 15 to 19, no overlap.
+square 11
+echo
+
+echo -n "No overlap (text newline)"
+
+
+
+# However, some sixel images end with a `-`, a Graphics New Line.
+# This can be useful for writing another image starting at the same
+# column without having to reposition the cursor.
+
+# Magenta square covers columns 51 to 60 and rows 5 to 9
+squaregnl 4 5 51
+# Cyan square is below magenta, rows 10 to 14, no overlap.
+squaregnl 5
+# Yellow square is below magenta, rows 15 to 19, no overlap.
+squaregnl 6
+
+echo -n "No overlap (graphics newline)"
+
+echo
+
