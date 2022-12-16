be5561e749d26d078e5754d7f94e471e28daa3f5
b9 <hackerb9@member.fsf.org>
2022-12-15 20:26:42 -0800

    Enable smooth scrolling mode if it's not already SET
    A genuine VT340 defaults to smooth scrolling enabled. However, it
    might have gotten disabled by user preference or by an overzealous
    terminfo "reset" sequence.
    
    Smooth scrolling will be RESET (disabled) when the script exits,
    if - and only if - it was RESET before the script started.

--- jerch/endless.sh
+++ jerch/endless.sh
@@ -4,9 +4,10 @@
 # Should print an endless sine curve, abort with Ctrl-C.
 # A genuine VT340 terminal updates this continuously. 
 
-# Always send ST to end sixel mode when script exits
+# Always send ST to end sixel mode when script exits.
+# If smooth scrolling was off before, reset it.
 ST=$'\e\\'
-trap 'echo -en "$ST"'  EXIT	
+trap 'echo -n "${ST}${resetsmooth}"'	EXIT
 
 period=200
 amplitude=50
@@ -14,7 +15,7 @@ amplitude=50
 sixels=(@$ A$ C$ G$ O$ _\$-)
 pi=3.141592653589793238
 
-# Number of samples (m*period) must be divisble by both $period and 6. 
+# Number of samples (m*period) must be divisible by both $period and 6. 
 declare -i m=1
 if ! factor $period 2>&- | cut -d: -f2- | grep -q -w 2; then
     m=m*2
@@ -22,6 +23,11 @@ fi
 if ! factor $period 2>&- | cut -d: -f2- | grep -q -w 3; then
     m=m*3
 fi
+
+# Enable smooth scrolling
+IFS=";$" read -a REPLY -t${timeout:-0.25} -s -p $'\e[?4$p' -d y
+if [[ ${REPLY[1]} == 2 ]]; then resetsmooth=$'\e[?4l'; fi
+echo -n $'\e[?4h'
 
 # Start sixels graphics string
 echo -ne "\x1bP0;0;0q\"1;1#1;2;100;0;0#1"
@@ -31,7 +37,7 @@ for (( y=0; y < m*period; y++ ))
 do
   x=$(echo "x=s(2*${pi}*${y}/${period})*${amplitude}+2*${amplitude}+0.5; scale=0; x/1" | bc -l)
   echo -ne "!${x}?${sixels[$((y%6))]}"		   	# 1.8 KBps (uncached)
-  f[y]=$(echo -ne "!${x}?${sixels[$((y%6))]}")		# 1.8 MBps (cached elts)
+  f[y]=$(echo -ne "!${x}?${sixels[$((y%6))]}")		# 1.8 MBps (cached elt)
   g+=$(echo -ne "!${x}?${sixels[$((y%6))]}")		# 88 MBps (string)
 done
 
