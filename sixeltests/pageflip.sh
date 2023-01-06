cde8cb11c4c6159ce8c90f28a5d1218ad0144aa4
b9 <hackerb9@member.fsf.org>
2023-01-06 01:12:18 -0800

    Add tput home to work around possible vt340 bug
    When sending the sixels to a page that is not visible, it scrolls the
    visible page. Is this just how text pages work? Do I need DECSDM mode?

--- sixeltests/pageflip.sh
+++ sixeltests/pageflip.sh
@@ -11,22 +11,39 @@
 
 CSI=$'\e['
 
+clear
+echo "This is page 1"
 echo "Please wait."
+tput home
 
 # Displayed page won't flip when cursor goes to page
-#echo ${CSI}'?64l'		# Page Cursor Coupling Mode off
+echo ${CSI}'?64l'		# Page Cursor Coupling Mode off
 echo ${CSI}'2 P'		# Page Position Absolute goto page 2
 
 # Draw an image on second page.
+clear
+echo "This is page 2"
 cat cat.six
 
-# Displayed page flips to where cursor is
+# Displayed page flips to where cursor is (page 2)
 echo ${CSI}'?64h'		# Page Cursor Coupling Mode: on
+
+tput home			# BUG: Why does page 1 scroll on sixel newlines
+
+# Now, do the same trick to write to first page while second page is viewed.
+# Displayed page won't flip when cursor goes to page
+echo ${CSI}'?64l'		# Page Cursor Coupling Mode off
+echo ${CSI}'1 P'		# Page Position Absolute goto page 1
+
+cat cat.six
+tput home
 
 # Wait 5 seconds or until a key is hit
 read -t5 -n1
 
-echo ${CSI}'1 P'		# Page Position Absolute. Goto page 1
+# Show page 1 again
+echo ${CSI}'?64h'		# Page Cursor Coupling Mode on
+
 
 # Stop flipping page if cursor switches to another page.
 #echo ${CSI}'?64l'		# Page Cursor Coupling Mode: off
