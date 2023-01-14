2df5cf00c632f274208e0bdd71e863d46c646714
b9 <hackerb9@member.fsf.org>
2023-01-13 21:20:44 -0800

    Discard stdin until terminal is finished sending
    We know it is finished because we request Device Attributes using
    Esc [ c.
    
    Note. This currently doesn't handle an 8-bit DA response.

--- charset/uplineload/uplineloadfont.c
+++ charset/uplineload/uplineloadfont.c
@@ -10,11 +10,9 @@
    o Presumes ST is sent at end of MediaCopy.
    o Should probably use select or poll to handle a timeout.
    o Could be lovelier on the screen as it is working.
-   o Takes 3 to 4 minutes to run and ^C doesn't cancel (stdio buffering).
+   o Takes 3 to 4 minutes to run.
 
    TODO:
-   o Use termios to set terminal to raw while running.
-   o On exit, reset terminal settings, flush stdout.
    o 132 column characters.
    o Double Width and Double Height characters.
    o Convert to downlineloadable font format.
@@ -38,6 +36,7 @@
 #include <signal.h>		/* signal() */
 
 int stty_setup( int fd );	/* Defined in setuptty.c */
+void discard_input ();
 int stty_restore( );
 
 /* Control Sequence Introducer */
@@ -107,7 +106,7 @@ char *receive_media_copy() {
   int delim='\e';		/* Esc \ is the String Terminator */
 
   /* Read data from terminal until Esc character. */
-  char c;
+  char c = '\0';
   while (c != 'P') {
     /* Skip everything until we get to a Device Control String (Esc P) */
     nread = getdelim(&line, &len, delim, stream); 
@@ -149,9 +148,99 @@ void save_region_to_file(char *filename, int x1, int y1, int x2, int y2) {
 }
 
 
+void
+discard_input() {
+  /* Read from stdin discarding any excess data the terminal is
+     sending. This works by sending the DA escape sequences (Esc [ c)
+     and waiting for the terminal to respond with Esc ... c
+
+     This is useful if the user hit ^C.
+
+	 Note: When a VT340 terminal is sending sixel data,
+	 it won't be listening to the keyboard while the
+	 "WAIT" light is on. The trick to get it to hear ^C
+	 is:
+
+	 1. Hit the Hold Screen button ("F1"),
+
+	 2. Wait a few seconds for the WAIT LED to go off
+y	    and the HOLD SCREEN LED to come on. (A white
+	    triangle also flashes in the upper left of the
+	    screen.)
+
+	 3. Press ^C
+
+	 4. Hit the Hold Screen button a second time to
+	    unpause the terminal
+
+     Why is this important here? Because this Hold Screen process adds
+     a significant delay to the response from the terminal. This rules
+     out the simple solution of waiting a few seconds for the terminal
+     to respond and then presuming it is done.
+
+     Instead, we send a DA request and then wait for that to
+     come back from the terminal.
+  */
+
+  /* Flush input and output buffers */
+  tcflush(STDIN_FILENO, TCIOFLUSH);
+
+  /* Ask the terminal to send Device Attributes (Esc [ ... c) */
+  printf("\e[c");
+
+  /* State machine to get DA response, skip DCS */
+  enum daresponse { BEGIN, GOTESC, GOTCSI, GOTDA };
+  enum daresponse state = BEGIN;
+
+  int c;
+  while ( state != GOTDA ) {
+    c = getchar();
+    if (c<0) break;		/* e.g., EOF */
+
+#ifdef DEBUG
+    if (c != '\e') printf("%c", c);  else printf("ESC");
+#endif
+
+    switch (state) {
+    case BEGIN:
+      if ( c == '\e' )
+	state = GOTESC;
+      break;
+    case GOTESC:
+      if ( c == '[' )
+	state = GOTCSI;
+      else if ( c == '\e' )
+	state = GOTESC;
+      else
+	state = BEGIN;
+      break;
+    case GOTCSI:
+      if ( c == 'c' )
+	state = GOTDA;
+      else if ( c == ';' || c == '?' || (c>='0' && c<='9') )
+	state = GOTCSI;
+      else if ( c == '\e' )
+	state = GOTESC;
+      else
+	state = BEGIN;
+      break;
+    default:
+      fprintf(stderr, "BUG. State should never be %d.\n", state);
+      _exit(1);
+    }	  
+  }
+
+  return;
+}
+
 static void
 cleanup(int signo) {
-  stty_restore();		/* Undo termios changes made by stty_setup()   */
+  fprintf(stderr, "\rDiscarding stdin from terminal...");
+  discard_input();
+  fprintf(stderr, "\r\e[K");	/* erase to end of line */
+
+  if (stty_restore() < 0)	/* Undo termios changes made by stty_setup() */
+    fprintf(stderr, "BUG! Why didn't tcsetattr work?");
   exit(0);
 }
 
@@ -198,6 +287,7 @@ int main() {
   }
 
   stty_restore(fileno(stdin));
+  printf("\n");
 
   return 0;
 }
