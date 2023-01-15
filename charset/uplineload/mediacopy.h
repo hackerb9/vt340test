c386bed0ec0c92b46b61e604fe5284728f553fde
b9 <hackerb9@member.fsf.org>
2023-01-15 13:25:29 -0800

    Separate mediacopy routines from main

---
+++ charset/uplineload/mediacopy.h
@@ -0,0 +1,12 @@
+#ifndef MEDIACOPY_H
+#define MEDIACOPY_H
+
+void setup_media_copy();
+char *receive_media_copy();
+void save_region_to_file(char *filename, int x1, int y1, int x2, int y2);
+
+char *csname(char *cs);
+char *cslongname(char *cs);
+
+#endif //MEDIACOPY_H
+
