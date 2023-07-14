184c5009d6a186f7a9b21ca415f1baada0907110
b9 <hackerb9@member.fsf.org>
2023-07-13 22:06:04 -0700

    Allow flipping back and forth between fonts
    For quick comparison of VT320 and VT340 fonts, added space and b keys.

--- charset/softfonts/fontpreview.sh
+++ charset/softfonts/fontpreview.sh
@@ -7,24 +7,48 @@ ESC=$'\e'
 DCS=${ESC}P
 ST=${ESC}\\
 
+declare -a files=("$@")
 
 position() {
+    # Move cursor to position number i
     local -i i="$1"
     tput cup $(( 2* (i/16) )) $(( 2* (i - 16*(i/16)) ))
 }    
 
-clear
-
-for f; do
-    i=20
-    tr ';' '\n' < "$f"  | while read; do
-	position ${i}
-	i=$(( i+1 ))
-	
-	echo "${DCS}q"
-	echo "${REPLY//\//-/}"
-	echo "${ST}"
-    done
+declare -i fn=0
+while :; do
+    f=${files[fn]}
+    if [[ -e "$f" ]]; then
+	clear
+	i=20			# XXX should read from file
+	tr ';' '\n' < "$f"  | while read; do
+	    position ${i}
+	    i=$(( i+1 ))
+	    
+	    echo "${DCS}q"
+	    echo "${REPLY//\//-/}"
+	    echo "${ST}"
+	done
+	echo "$f"
+    else
+	echo "Error: '$f' does not exist" >&2
+    fi
     echo
+    echo "      [Q] Quit"
+    echo "  [Space] Next" 
+    echo "      [B] Back"
+    read -n1 -s
+    case "$REPLY" in
+	q|Q|$'\e') exit ;;
+	" "|n) fn=fn+1
+	       if (( fn >= ${#files[@]} )); then exit; fi
+	       echo "fn is $fn and num files is ${#files}"
+	       ;;
+	b|B|p) fn=fn-1
+	       if (( fn < 0 )); then fn=0; fi
+	       ;;
+	*) echo "Unknown key '$REPLY'"
+	   ;;
+    esac
 done
     
