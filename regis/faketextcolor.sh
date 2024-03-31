2cb793f9055dd08f205f2d86a50976e006e8989b
b9 <hackerb9@member.fsf.org>
2024-03-31 13:07:24 -0700

    Add more tests of moving colorized text using escape sequences

--- regis/faketextcolor.sh
+++ regis/faketextcolor.sh
@@ -40,37 +40,59 @@ echo
 echo "Total colorization time: $duration seconds."
 
 
+######################################################################
 
 # Some basic tests of what happens when the colorized text is moved.
-echo
-read -n1 -p "Hit a key to test shifting text left."
-echo "Testing delete."
-echo $'\e7'			# Save cursor
-tput cup 10 0			# CUrsor Position
-tput dch 5			# Delete CHaracter (five)
-echo $'\e8'			# Restore cursor
+echo; echo
+tput sc				# Save cursor (Esc 7 on VT340)
 
+i=0
+echo -n "$((++i)). "
+read -s -n1 -p "Hit a key to test shifting text down (reverse index)"
+tput cup 0 0
+tput ri				# Reverse Index (Esc M)
+
+tput rc				# Restore cursor
+tput cud1			# Down +1
+tput el				# Clear to end of line
+echo -n "$((++i)). "
+
+read -s -n1 -p "Hit a key to test shifting text up. (newline)"
+tput cup 1000 0			# CUrsor Position to last row, column 0
 echo
-read -n1 -p "Hit a key to test shifting text right"
-echo "Testing insert."
-tput sc				# Save cursor
+
+tput rc; tput el
+echo -n "$((++i)). "
+
+read -s -n1 -p "Hit a key to test shifting text down (insert line)"
+tput cup 11 0
+tput il1			# Insert Line 
+
+tput rc; tput cud1; tput el
+echo -n "$((++i)). "
+
+read -s -n1 -p "Hit a key to test shifting text up. (delete line)"
+tput cup 11 0
+tput dl1			# Delete line
+
+tput rc; tput el
+echo -n "$((++i)). "
+
+### The VT340 does not retain color after the following tests.
+
+read -s -n1 -p "Hit a key to test shifting text left. (delete char)"
+tput cup 10 0			# CUrsor Position to row 10, column 0
+tput dch 5			# Delete CHaracter (five)
+
+tput rc
+tput el
+echo -n "$((++i)). "
+
+read -s -n1 -p "Hit a key to test shifting text right. (insert char)"
 tput cup 12 0
 tput smir			# Start Mode InseRt
 echo -n "     "			# Five spaces
 tput rmir			# Remove Mode InseRt
-tput rc				# Restore cursor
 
-echo
-read -n1 -p "Hit a key to test shifting text down"
-echo "Testing reverse index."
-echo $'\e7'			# Save cursor
-tput cup 11 0
-tput ri				# Reverse Index
-echo $'\e8'			# Restore cursor
-tput cud1			# Down +1
-
-echo
-echo "Hit ENTER to test scrolling up"
-
-# Put cursor on last line for scroll test
+# Put cursor on last line
 tput cup 1000 0
