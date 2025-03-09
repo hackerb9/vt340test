f45db2d0e4b664b71f2d9d6344f83e2965b858b6
b9 <hackerb9@member.fsf.org>
2025-03-08 20:59:30 -0800

    Test changing double-size with graphics and no text
    Actually, try all the permutations of Text, Graphics, and Attributes.
    Turns out the VT340 clears graphics whenever attributes are changed,
    even if there is no text on that line.

--- regis/regis-decdwl.sh
+++ regis/regis-decdwl.sh
@@ -1,68 +1,105 @@
 #!/bin/bash
 
 # A test of the interaction of ReGIS graphics upon line attributes.
-# (DECDWL - Double Width Line, DECDHL - Double Height Lines).
+# Although only DECDWL is shown, the effect for DECDHL is the same.
+
+#  DECDWL - Double Width Line - Esc # 6
+#  DECDHL - Double Height Lines - Esc # 3 and Esc # 4
+#  DECSWL - Single Width Line - Esc # 5
+
+# On a VT340, changing the line attributes clears any graphics on that row.
+# (Setting line attributes to the same as before has no effect.)
 
 # Unlike sixel graphics, ReGIS does not reset the line attribute flags
 # to single width nor does it clear the underlying text buffer.
 # (This is a good thing as it helps wide text and graphics coexist.)
 
+# This tests all orderings of Line Attributes, Text, and Graphics.
+#
+# The most natural ordering is to set the attributes immediately
+# before the text, with the graphics coming before or after:
+# 1. ATG
+# 2. GAT
+# Less natural would be to set the attributes immediately after the text:
+# 3. TAG
+# 4. GTA
+# Most peculiar would be to draw graphics between the attributes and text:
+# 5. AGT
+# 6. TGA
+
+# Although peculiar, #5 is useful because it means it is possible to
+# update the text on a double-width line without erasing the graphics.
+
+
 # Define a macro, @C, to draw a 120x120 flag
 echo -n $'\eP0p'
 cat <<-EOF
-	@:Q
-	    F(
-	      V(B)
-	       [+120,]
-	       [,+24]
-	       [-120,]
-	       (E)
-	    )
-	    V[,+24]
-	@;
-	@:C
-	    W(I(B)) @Q
-	    W(I(W)) @Q
-	    W(I(R)) @Q
-	    W(I(W)) @Q
-	    W(I(B)) @Q
-	@;
+;"Zulu. Natural flag size is 2x2 pixels. Use multiplier 60 for 120x120."
+@:Z
+    W(M60)
+    P7
+    W(I(Y))    F( V 3005 )
+    W(I(B))    F( V 1663 )
+    W(I(R))    F( V 7441 )
+    W(I(D))    F( V 5227 )
+    P3
+    W(I(W))    V 00664422
+@;
 EOF
 echo -n $'\e\\'
 
+
+DECDWL=$'\e#6'
+
 clear
-tput cup 2 5
-echo "A test of the interaction of ReGIS graphics upon line attributes."
+tput cup 1
+cat <<EOF
+		The interaction of ReGIS graphics with
+		  line attributes (double-size text)
+EOF
 
-tput cup 5 20
-echo $'\e#5 1.   single-width (normal)'
-
-tput cup 6 10
-echo $'\e#62. double-width'
+tput cup 5 10
+echo ${DECDWL}"1. Attributes, Text, Graphics"
 
 tput cup 7 10
-echo $'\e#33. double-height'
-tput cup 8 10
-echo $'\e#43. double-height'
+echo "3. Text, Attributes, Graphics"${DECDWL}
 
-tput cup 10
-echo $'\e#6'
-
-sleep 1
-
-# Draw the flag starting at line 6
-echo -n $'\eP0p'
-echo -n "P[50,100]@C"
-echo -n $'\e\\'
-
-sleep 1
+tput cup 9			# Line 5. AGT
+echo ${DECDWL}
 
 tput cup 10 10
-echo "4. text added afterward"
+echo "6. Text, Graphics, Attributes"
+
+
+sleep .5
+
+# Draw the flag using macro starting at line 6
+echo -n $'\eP0p'
+echo -n "P[50,100]@Z"
+echo -n $'\e\\'
+
+sleep .5
+
+tput cup 6 10
+echo ${DECDWL}"2. Graphics, Attributes, Text"
+
+tput cup 8 10
+echo "4. Graphics, Text, Attributes"${DECDWL}
+
+tput cup 9 10
+echo "5. Attributes, Graphics, Text"
+
+tput cup 10			# Line 6. TGA
+echo ${DECDWL}
+
 
 tput cup 12 0
-echo "Note: Line 4 was set to double-width before the graphic was shown."
-echo "Changing line attributes clears graphics but resizes text."
+cat <<EOF
+Note:
 
-tput cup 18 0
+Even numbered lines set the line attributes after drawing ReGIS graphics.
+On a genuine VT340, the graphics on those lines are erased, regardless of
+whether there was text on the line or not.
+
+EOF
 
