b965399e91569a92ac2b93598afcec12a7e2ce45
b9 <hackerb9@member.fsf.org>
2025-03-11 13:57:00 -0700

    Add VT240 compatibility

--- sixeltests/sixel-decdwl.sh
+++ sixeltests/sixel-decdwl.sh
@@ -32,63 +32,48 @@
 # 6. TGA
 
 
-
-# Define a sixel image to draw a 120x120 flag
-Y='P//~
-TITLE=Yankee
-\P//~
-COMMENT=The letter Y represented using the International Code of Signals.
-\P//~
-CREATOR=James Holderness
-\P//~
-URL=https://github.com/j4james/vtinterco/
-\P9;1q"20;1#2!120~$"1;1#6!19~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@-!13~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@-!7~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@-~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@-!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}-!13?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!6~-!7?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!12~-?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!18~-!19~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@-!13~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@-!7~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@-~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@-!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}-!13?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!6~-!7?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!12~-?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!18~-!19~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@-!13~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@-!7~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@-~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@\'
-
-DECDWL=$'\e#6'
-
-clear
-tput cup 1
-cat <<EOF
+main() {
+    init
+    clear
+    tput cup 1
+    cat <<EOF
 	      The effect of ordering upon sixel graphics
 	       with line attributes (double-size text)
 EOF
 
-tput cup 5 10
-echo ${DECDWL}"1. Attributes, Text, Graphics"
+    tput cup 5 10
+    echo ${DECDWL}"1. Attributes, Text, Graphics"
 
-tput cup 7 10
-echo "3. Text, Attributes, Graphics"${DECDWL}
+    tput cup 7 10
+    echo "3. Text, Attributes, Graphics"${DECDWL}
 
-tput cup 9			# Line 5. AGT
-echo ${DECDWL}
+    tput cup 9			# Line 5. AGT
+    echo ${DECDWL}
 
-tput cup 10 10
-echo "6. Text, Graphics, Attributes"
+    tput cup 10 10
+    echo "6. Text, Graphics, Attributes"
 
+    sleep .5
 
-sleep .5
+    # Draw the flag. (Hardcoded to column 6, row 6.)
+    echo -n "$Y"
 
-# Draw the flag using macro starting at line 6
-echo $'\ePq\e\\'
-tput cup 5 5
-echo -n "$Y"
+    sleep .5
 
-sleep .5
+    tput cup 6 10
+    echo ${DECDWL}"2. Graphics, Attributes, Text"
 
-tput cup 6 10
-echo ${DECDWL}"2. Graphics, Attributes, Text"
+    tput cup 8 10
+    echo "4. Graphics, Text, Attributes"${DECDWL}
 
-tput cup 8 10
-echo "4. Graphics, Text, Attributes"${DECDWL}
+    tput cup 9 10
+    echo "5. Attributes, Graphics, Text"
 
-tput cup 9 10
-echo "5. Attributes, Graphics, Text"
+    tput cup 10			# Line 6. TGA
+    echo ${DECDWL}
 
-tput cup 10			# Line 6. TGA
-echo ${DECDWL}
-
-tput cup 12 0
-cat <<EOF
+    tput cup 12 0
+    cat <<EOF
 The VT340 resets all line attributes to single-width and clears the
 underlying text buffer when a sixel image is received. Existing text
 is retained only in the bitmap buffer and cannot be edited.
@@ -99,3 +84,128 @@ Line 5 has the text printed on top of the image because the double-
 width attribute that makes columns twice as wide was reset.
 
 EOF
+}
+
+
+
+init() {
+
+    DECDWL=$'\e#6'
+
+    # Define a sixel image to draw a 120x120 flag at text column 6,
+    # row 6. For compatibility with the VT240, this image uses DECSDM
+    # (Sixel Display Mode) to position it absolutely on the screen
+    # instead of relative to the text cursor.
+
+    Y='P//~
+TITLE=Yankee
+\P//~
+COMMENT=The letter Y represented using the International Code of Signals.
+\P//~
+CREATOR=James Holderness
+\P//~
+URL=https://github.com/j4james/vtinterco/
+\P9;1q
+"1;1----- ----- ----- --!50?
+"20;1#2!120~$
+"1;1
+!50?#6!19~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@-
+!50?!13~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@-
+!50?!7~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@-
+!50?~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@-
+!50?!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}-
+!50?!13?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!6~-
+!50?!7?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!12~-
+!50??_ow{}!19~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!18~-
+!50?!19~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@-
+!50?!13~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@-
+!50?!7~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@-
+!50?~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@-
+!50?!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}-
+!50?!13?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!6~-
+!50?!7?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!12~-
+!50??_ow{}!19~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!18~-
+!50?!19~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@-
+!50?!13~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@-
+!50?!7~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@-
+!50?~^NFB@!19?_ow{}!19~^NFB@!19?_ow{}!19~^NFB@\'
+
+    # For VT240 compatibility, we need to be able to get and set DECSDM
+    CSI=$'\e['			# Control Sequence Introducer
+    DCS=$'\eP'			# Device Control String
+    ST=$'\e\\'			# String Terminator
+    DECSDM=80			# Mode number for get/set/reset mode
+    oldmode=$(getmode $DECSDM)
+    setmode $DECSDM
+    trap cleanup EXIT
+}
+
+cleanup() {
+    setmode $DECSDM $oldmode
+}
+
+getmode() {
+    # Inquires if mode in $1 is set or reset in the terminal.
+    # Prints an integer as result:
+    #   0: "not recognized"
+    #   1: "set"
+    #   2: "reset"
+    #   3: "permanently set"
+    #   4: "permanently reset"
+    #
+    mode=${1:-80}
+    if ! IFS=";$" read -a REPLY -t 0.25 -s -p ${CSI}'?'$mode'$p' -d y; then
+	# Terminal did not respond to inquiry for mode.
+	echo 0			# Pretend terminal responded, "not recognized"
+    else
+	echo "${REPLY[1]}"
+    fi
+}
+
+setmode() {
+    # Sets terminal mode in $1 to value in $2
+    #
+    # If $2 is not given, $2 defaults to "h" (high == SET).
+    # ...   starts with the letter "h" (high) the mode is SET.
+    # ...   starts with the letter "l" (low) the mode is RESET.
+    # ...   is an integer from 0 to 4, then $2 is looked up in this table:
+    #
+    #	       0: "not recognized"	-> "l"
+    #	       1: "set"			-> "h"
+    #	       2: "reset"		-> "l"
+    #	       3: "permanently set"	-> "h"
+    #	       4: "permanently reset"	-> "l"
+    #
+    #       (This allows the result from getmode() to be used as input).
+
+    mode=${1:-80}		# Default mode is 80 (DECSDM)
+    if [[ $mode -le 0 ]]; then
+	echo "Bug: mode must be a number greater than 0, not '$mode'" >&2
+	exit 1
+    fi
+
+    case "$2" in
+	"")    value=h		# $2 is empty
+	       ;;
+	l*|L*) value=l		# Letter "l"
+	       ;;
+	h*|H*) value=h
+	       ;;
+	0|2|4) value=l
+	       ;;
+	1|3)   value=h		# Number 1
+	       ;;
+	reset) value=l
+	       ;;
+	set)   value=h
+	       ;;
+	*) echo "Unknown value '$2' passed to setmode()" >&2
+	   exit 1
+	   ;;
+    esac
+
+    # Okay, now set or reset the mode.
+    echo -n ${CSI}'?'$mode$value
+}
+
+main "$@"
