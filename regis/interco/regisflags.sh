8cdc5cdf8d2f216359a2ab3933e51ff455b165a3
b9 <hackerb9@member.fsf.org>
2025-03-11 18:11:51 -0700

    Add Interco flag as loading meter
    Loading the macros takes about some time (five seconds), so show a
    meter so the user knows the program isn't stuck.

--- regis/interco/regisflags.sh
+++ regis/interco/regisflags.sh
@@ -6,6 +6,7 @@ ST=$'\e\\'
 trap cleanup EXIT
 cleanup() {
     echo -n ${ST}
+    stty echo
 }
 
 
@@ -25,19 +26,43 @@ scale() {
 	  scale = 0; ($n - r)/1" | bc -l
 }    
 
-
-main() {
+loadflags() {
+    # Load all the macros into the VT340's memory for faster drawing.
+    # For details on the interspersed gobbledygook, see the file interco.regis.
     echo -n ${REGIS}
-    echo ';P[0,0]W(I(R))T"Please wait, loading macros into terminal"'
-
-    cat alfa.regis bravo.regis charlie.regis delta.regis echo.regis \
-        foxtrot.regis golf.regis hotel.regis india.regis juliet.regis \
-	kilo.regis lima.regis mike.regis november.regis oscar.regis \
-	papa.regis quebec.regis romeo.regis sierra.regis tango.regis \
-	uniform.regis victor.regis whiskey.regis xray.regis yankee.regis \
-	zulu.regis
+    echo ";S(E)W(M1)P[0,360]"
+    echo 'W(I(R))T(S2)"Please wait, loading macros into terminal."T(S1)'
+    cat alfa.regis bravo.regis charlie.regis
+    echo "W(M20)P[0,0]"
+    cat delta.regis echo.regis
+    echo "W(I(R))F(V(B)P666666666666666V[]P200000000V[]P2222222222222V[]V(E))"
+    cat	foxtrot.regis golf.regis hotel.regis
+    echo "P600000000"
+    cat	india.regis juliet.regis
+    echo "W(I(W))F(V(B)P6666666666666V[]P200000000V[]P22222222222V[]V(E))"
+    cat	kilo.regis lima.regis mike.regis
+    echo "P 6 0000 0000"
+    cat november.regis oscar.regis
+    echo "W(I(R))F(V(B)P 666 66666 666V[]P 2 0000 0000V[]P 22 22222 22V[]V(E))"
+    cat papa.regis quebec.regis romeo.regis 
+    echo "P 6 0000 0000"
+    cat sierra.regis tango.regis
+    echo "W(I(W))F(V(B)P 66 66666 66V[]P 2 0000 0000V[]P 2 22222 2V[]V(E))"
+    cat uniform.regis victor.regis whiskey.regis
+    echo "P 6 0000 0000"
+    cat  xray.regis yankee.regis
+    echo "W(I(R))F(V(B)P 6 66666 6V[]P 2 0000 0000V[]P 22222V[]V(E))"
+    cat zulu.regis
 
     echo -n ${ST}
+}
+
+
+main() {
+    stty -echo			# Don't interpret stray keystrokes as ReGIS
+    loadflags
+    # Clear screen with dark grey.
+    echo -n "${REGIS}W(I8)F(V[0,0][+799][,+479][-799][,-479])${ST}"
 
     local -A natsize=( [A]=4 [B]=4 [C]=5 [D]=4 [E]=2 [F]=2 [G]=6 [H]=2
 		       [I]=4 [J]=3 [K]=2 [L]=2 [M]=6 [N]=4 [O]=1 [P]=5
