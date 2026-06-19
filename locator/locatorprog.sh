8fe3abeb507ad456dc31d56984f2ea2abc4b7272
b9 <hackerb9@member.fsf.org>
2026-06-18 23:26:39 -0700

    Stub

---
+++ locator/locatorprog.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Test programmability of VT340 locator device buttons.
+
+# Programming the keys using DECLKD allows for detecting button-up,
+# not just down, which means click and drag should be possible.
+   
+# Any of the four locator buttons can send a report containing six
+# bytes when pressed or released.
+
